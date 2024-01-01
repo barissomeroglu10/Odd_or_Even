63d570bd2a36ed1dbac0627ee7596defb4b5f354
baris <baris@BARIS_SOMEROGLU>
2024-01-01 14:23:31 +0300

    Add project files.

---
+++ +or-/Odd_Even.cpp
@@ -0,0 +1,32 @@
+﻿/*
+In this code we will get a number from user and will check number is odd or even.
+
+Developer: Barış Someroğlu
+Date: 01.01.2024
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number;
+
+	
+	cout << "Please Enter the Number: ";
+	cin >> Number;
+
+	if (Number % 2 == 0)
+	{
+		cout << "Your Number is Even!" << endl;
+	}
+
+	else
+	{
+		cout << "Your Number is Odd!" << endl;
+	}
+	
+
+	return 0;
+}
